798467c4a1517ea41dd5536ce3755a4cc48102c3
Hleb Paulowicz <hpaulowicz@gmail.com>
2017-09-30 21:17:22 +0300

    Grade-passed fibonacci_last_digit

--- introduction_starter/fibonacci_last_digit/fibonacci_last_digit.py
+++ introduction_starter/fibonacci_last_digit/fibonacci_last_digit.py
@@ -1,19 +1,16 @@
 # Uses python3
 import sys
 
-def get_fibonacci_last_digit_naive(n):
-    if n <= 1:
-        return n
-
-    previous = 0
-    current  = 1
-
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-
-    return current % 10
+def get_fibonacci_last_digit(n):
+    arr = [0] * (n + 2)
+    arr[0] = 0
+    arr[1] = 1
+    for i in range(2, n + 1):
+        arr[i] = ((arr[i - 2] % 10) + (arr[i - 1] % 10)) % 10
+    return arr[n]
 
 if __name__ == '__main__':
+    # input = input()
     input = sys.stdin.read()
     n = int(input)
-    print(get_fibonacci_last_digit_naive(n))
+    print(get_fibonacci_last_digit(n))
