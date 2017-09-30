0d2959a1184c2f75ba742ce77f0a72ecb7bf73b0
Hleb Paulowicz <hpaulowicz@gmail.com>
2017-09-30 21:53:38 +0300

    LCM

--- introduction_starter/lcm/lcm.py
+++ introduction_starter/lcm/lcm.py
@@ -1,15 +1,16 @@
 # Uses python3
 import sys
 
-def lcm_naive(a, b):
-    for l in range(1, a*b + 1):
-        if l % a == 0 and l % b == 0:
-            return l
-
-    return a*b
+def lcm(a, b):
+    in_a = a
+    in_b = b
+    while b:
+        a, b = b, int(a%b)
+    return int((in_b / a) * in_a)
 
 if __name__ == '__main__':
+    # input = raw_input()
     input = sys.stdin.read()
     a, b = map(int, input.split())
-    print(lcm_naive(a, b))
+    print(lcm(a, b))
 
