c88bb7df65769c0062989827e9d915df5bafd284
Hleb Paulowicz <hpaulowicz@gmail.com>
2017-09-30 20:49:57 +0300

    Grade-passed fibonacci.py

--- introduction_starter/fibonacci/fibonacci.py
+++ introduction_starter/fibonacci/fibonacci.py
@@ -1,9 +1,17 @@
 # Uses python3
+
+import sys
+
 def calc_fib(n):
-    if (n <= 1):
-        return n
+    arr = [0] * (n+2)
+    arr[0] = 0
+    arr[1] = 1
+    for i in range(2, n+1):
+        arr[i] = arr[i-2] + arr[i-1]
+    return arr[n]
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
-
-n = int(input())
-print(calc_fib(n))
+if __name__ == "__main__":
+    input = input()
+    # input = sys.stdin.read()
+    n = int(input)
+    print(calc_fib(n))
--- introduction_starter/gcd/gcd.py
+++ introduction_starter/gcd/gcd.py
@@ -2,13 +2,11 @@
 import sys
 
 def gcd_naive(a, b):
-    current_gcd = 1
-    for d in range(2, min(a, b) + 1):
-        if a % d == 0 and b % d == 0:
-            if d > current_gcd:
-                current_gcd = d
-
-    return current_gcd
+    result = 0
+    for i in range(1, a + b):
+        if a % i == 0 and b % i == 0:
+            result = i
+    return result
 
 if __name__ == "__main__":
     input = sys.stdin.read()
