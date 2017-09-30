8ad54786a706495f6d5005db3f0cce777cbca771
Hleb Paulowicz <hpaulowicz@gmail.com>
2017-09-30 21:23:43 +0300

    Grade-passed gcd

--- introduction_starter/gcd/gcd.py
+++ introduction_starter/gcd/gcd.py
@@ -1,14 +1,13 @@
 # Uses python3
 import sys
 
-def gcd_naive(a, b):
-    result = 0
-    for i in range(1, a + b):
-        if a % i == 0 and b % i == 0:
-            result = i
-    return result
+def euclide_gcd(a, b):
+    while b:
+        a, b = b, a%b
+    return a
 
 if __name__ == "__main__":
+    # input = input()
     input = sys.stdin.read()
     a, b = map(int, input.split())
-    print(gcd_naive(a, b))
+    print(euclide_gcd(a, b))
