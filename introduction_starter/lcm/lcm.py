4b257d069ebbfc583436dbf010a3ce82491c2822
Hleb Paulowicz <hpaulowicz@gmail.com>
2017-10-01 21:14:52 +0300

    If the result is 46374212988031352: You are using floating point math, because you used the / true division operator. Floating point can only approximate large numbers, and the difference yo use is a result of that.
    Use // floor division instead
    
    Grade-passed solution

--- introduction_starter/lcm/lcm.py
+++ introduction_starter/lcm/lcm.py
@@ -6,11 +6,11 @@ def lcm(a, b):
     in_b = b
 
     while b:
-        a, b = int(b), int(a%b)
-    return int((in_b / a) * in_a)
+        a, b = b, a%b
+    return int((in_b // a) * in_a)
 
 if __name__ == '__main__':
-    input = raw_input()
-    # input = sys.stdin.read()
+    # input = input()
+    input = sys.stdin.read()
     a, b = map(int, input.split())
     print(lcm(a, b))
