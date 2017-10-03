c981bc52d44f0a85902c9e1bbb357ed29527349e
Hleb Paulowicz <hpaulowicz@gmail.com>
2017-10-03 11:51:29 +0300

    Fibonacci huge unexpected small solution

--- introduction_starter/fibonacci_huge/fibonacci_huge.py
+++ introduction_starter/fibonacci_huge/fibonacci_huge.py
@@ -2,21 +2,24 @@
 import sys
 
 def get_fibonacci_huge(n, m):
-    arr = [0] * 2
     if n > 0:
-        arr[0] = 0
-        arr[1] = 1
-        sequence = [0, 1,]
-        for i in range(2, n + 1):
-            fn = arr[-2] + arr[-1]
-            arr += [fn]
-            sequence.insert(len(sequence), int(arr[-1] % m))
-            if sequence[-3:] == [0, 1, 1] and i > 2:
-                return arr[n % len(sequence[0:-3])] % m
-    return arr[-1] % m
+        fibonacci = [0, 1, 1]
+        sequence = [0, 1, 1]
+
+        for i in range(3, n + 1):
+            fibonacci.append(((fibonacci[-2] % m) + (fibonacci[-1] % m)) % m)
+
+            sequence[0] = fibonacci[-3]
+            sequence[1] = fibonacci[-2]
+            sequence[2] = fibonacci[-1]
+
+            if sequence == [0, 1, 1]:
+                return fibonacci[n % (i - 2)] % m
+
+        return fibonacci[-1] % m
 
 if __name__ == '__main__':
-    # input = input()
-    input = sys.stdin.read();
+    input = input()
+    # input = sys.stdin.read()
     n, m = map(int, input.split())
     print(get_fibonacci_huge(n, m))
