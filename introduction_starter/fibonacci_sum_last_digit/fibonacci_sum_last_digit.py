5db8822cb5df4996f131b2c11b7ad72826899bf1
Hleb Paulowicz <hpaulowicz@gmail.com>
2017-10-04 10:50:13 +0300

    False

--- introduction_starter/fibonacci_sum_last_digit/fibonacci_sum_last_digit.py
+++ introduction_starter/fibonacci_sum_last_digit/fibonacci_sum_last_digit.py
@@ -2,22 +2,31 @@
 import sys
 
 def fibonacci_sum(n):
-    arr = [0] * 2
-    s = 0
     if n > 0:
-        arr[0] = 0
-        arr[1] = 1
-        s += 1
-        for i in range(2, n + 1):
-            # fn = arr[-2] + arr[-1]
-            fn = ((arr[-2] % 10) + (arr[-1] % 10)) % 10
-            arr += [fn]
-            s += fn
-            del arr[0]
-    return s % 10
+        fibonacci = [0, 1, 1]
+        sequence = [0, 1, 1]
+
+        m = 10
+
+        for i in range(3, n + 1):
+            fibonacci.append(((fibonacci[-2] % m) + (fibonacci[-1] % m)) % m)
+
+            sequence[0] = fibonacci[-3]
+            sequence[1] = fibonacci[-2]
+            sequence[2] = fibonacci[-1]
+
+            print(fibonacci)
+            print(sequence)
+
+            if sequence == [0, 1, 1]:
+                return fibonacci[n % (i - 2)] % m
+
+        return fibonacci[-1] % m
+    else:
+        return 0
 
 if __name__ == '__main__':
-    # input = input()
-    input = sys.stdin.read()
+    input = input()
+    # input = sys.stdin.read()
     n = int(input)
     print(fibonacci_sum(n))
