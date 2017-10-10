8d5ee3bb6f9596907d0a6e8f24e03488baa153a7
Hleb Paulowicz <hpaulowicz@gmail.com>
2017-10-10 11:57:28 +0300

    Grade-passed change.py

--- greedy_algorithms/change/change.py
+++ greedy_algorithms/change/change.py
@@ -3,8 +3,18 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m
+    values = [1, 5, 10]
+    items = 0
+    change = m
+    for i in range(0, len(values)):
+        max_value = max(values)
+        rem = change // max_value
+        change = change - (max_value * rem)
+        values.remove(max_value)
+        items += rem
+    return items
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
+    # m = int(input())
     print(get_change(m))
