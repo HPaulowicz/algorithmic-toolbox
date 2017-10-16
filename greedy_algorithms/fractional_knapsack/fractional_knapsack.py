c1c92ef37ee5c1f3b2f218b78f21bd7ce62bba64
Hleb Paulowicz <hpaulowicz@gmail.com>
2017-10-16 16:21:37 +0300

    Passed

--- greedy_algorithms/fractional_knapsack/fractional_knapsack.py
+++ greedy_algorithms/fractional_knapsack/fractional_knapsack.py
@@ -6,39 +6,41 @@ def get_optimal_value(capacity, weights, values):
     vpus = []
     for i in range(0, len(values)):
         vpu = float(values[i] / weights[i])
-        vpus.append(vpu)
+        vpus.append([vpu, values[i], weights[i]])
 
     remaining_capacity = capacity
 
-    while remaining_capacity > 0:
-        # print(remaining_capacity)
-        index = vpus.index(max(vpus))
-        max_items = remaining_capacity // weights[index]
-        mod = remaining_capacity % weights[index]
-        print(max_items, mod)
-        if max_items > 0:
-            units_weight = weights[index] * max_items
-            remaining_capacity = remaining_capacity - units_weight
-            value = float(value + (values[index] * max_items))
-        else:
-            units_weight = (remaining_capacity / weights[index]) * weights[index]
-            remaining_capacity = remaining_capacity - units_weight
-            value = float(value + (vpus[index] * capacity))
+    sorted_values = sorted(vpus, key=lambda x: x[0], reverse=True)
 
-        del weights[index]
-        del values[index]
-        del vpus[index]
+    for i in range(0, len(sorted_values)):
+        if remaining_capacity > 0:
+
+            wgt = sorted_values[i][2]
+            val = sorted_values[i][1]
+            vpu = sorted_values[i][0]
+
+            item_remainder = remaining_capacity // wgt
+
+            if item_remainder > 0:
+                remaining_capacity = remaining_capacity - wgt
+                value = value + val
+            else:
+                remaining_capacity = 0
+                value = value + (vpu * capacity)
 
     return round(value, 4)
 
 
 if __name__ == "__main__":
-    # data = list(map(int, sys.stdin.read().split()))
-    data = list(map(int, input().split()))
+    data = list(map(int, sys.stdin.read().split()))
+    # data = list(map(int, input().split()))
     n, capacity = data[0:2]
     values = data[2:(2 * n + 2):2]
     weights = data[3:(2 * n + 2):2]
     opt_value = get_optimal_value(capacity, weights, values)
     print("{:.10f}".format(opt_value))
 
-#     3 50 60 20 100 50 120 30 ------ 1 10 500 30 ------ 1 1000 500 30
+# 3 50 60 20 100 50 120 30
+# 1 10 500 30
+# 1 1000 500 30
+# 3 100 60 20 100 50 120 30
