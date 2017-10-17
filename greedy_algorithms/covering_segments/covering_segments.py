e49dce98bf41fb75af2ba2556c1c9951eee8d6c5
Hleb Paulowicz <hpaulowicz@gmail.com>
2017-10-17 15:19:28 +0300

    initial new

--- greedy_algorithms/covering_segments/covering_segments.py
+++ greedy_algorithms/covering_segments/covering_segments.py
@@ -7,16 +7,21 @@ Segment = namedtuple('Segment', 'start end')
 def optimal_points(segments):
     points = []
     #write your code here
+    print(segments)
     for s in segments:
         points.append(s.start)
         points.append(s.end)
     return points
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
+    # input = sys.stdin.read()
+    input = input()
     n, *data = map(int, input.split())
     segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
     points = optimal_points(segments)
     print(len(points))
     for p in points:
         print(p, end=' ')
+
+# 3 1 3 2 5 3 6
+# 4 4 7 1 3 2 5 5 6
--- greedy_algorithms/different_summands/different_summands.py
+++ greedy_algorithms/different_summands/different_summands.py
@@ -7,9 +7,14 @@ def optimal_summands(n):
     return summands
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
+    # input = sys.stdin.read()
+    input = input()
     n = int(input)
     summands = optimal_summands(n)
     print(len(summands))
     for x in summands:
         print(x, end=' ')
+
+#
+#
+#
