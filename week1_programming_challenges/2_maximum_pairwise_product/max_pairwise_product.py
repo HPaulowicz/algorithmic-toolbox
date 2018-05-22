4780ae4c134d6b99da63b6688bd15dffb5099a2f
Hleb Paulowicz <hpaulowicz@gmail.com>
2018-05-22 14:43:51 +0300

    Max pairwise product

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -1,12 +1,8 @@
 # Uses python3
 
 def mpp(n, a):
-    result = 0
-    for i in range(0, n):
-        for j in range(i+1, n):
-            if a[i]*a[j] > result:
-                result = a[i]*a[j]
-    return result
+    a = sorted(a)
+    return a[n-1]*a[n-2]
 
 if __name__ == '__main__':
     n = int(input())
